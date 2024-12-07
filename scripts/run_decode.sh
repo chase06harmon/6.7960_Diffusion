ae904a4650292ebfd919d1cf399d2601b792e1bd
chaseharmon <chase1harmon@gmail.com>
2024-12-07 02:25:43 -0500

    decode stuff

--- scripts/run_decode.sh
+++ scripts/run_decode.sh
@@ -1,4 +1,4 @@
 python -u run_decode.py \
---model_dir diffusion_models/{name-of-model-folder} \
+--model_dir diffusion_models/diffuseq_EnglishSlang_h128_lr0.0001_t2000_sqrt_lossaware_seed102_learned_mask_fp16_denoise_0.5_reproduce20241207-04:26:36 \
 --seed 123 \
 --split test
