c43bbc1521111314a4cb9dc409cff7592e6cb9b7
harmonc <harmonc@mit.edu>
2024-12-06 23:09:14 -0500

    changes

--- scripts/train.sh
+++ scripts/train.sh
@@ -9,7 +9,7 @@ CUDA_VISIBLE_DEVICES=1,2 python -m torch.distributed.launch --nproc_per_node=4 -
 --bsz 425 \
 --microbatch 425 \
 --dataset EnglishSlang \
---data_dir /nobackup/users/djtufto/6.7960_Diffusion/datasets/EnglishSlang \
+--data_dir ./datasets/EnglishSlang \
 --learned_mean_embed True \
 --denoise True \
 --vocab bert \
