5284987f35367325fa07d36335758b27cf97437a
harmonc <harmonc@mit.edu>
2024-12-06 23:23:09 -0500

    logger

--- scripts/train.sh
+++ scripts/train.sh
@@ -17,4 +17,5 @@ CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=4 -
 --use_fp16 \
 --denoise_rate 0.5 \
 --schedule_sampler lossaware \
---notes learned_mask_fp16_denoise_0.5_reproduce
+--notes learned_mask_fp16_denoise_0.5_reproduce \
+--log_interval 100
