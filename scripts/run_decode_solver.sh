57e3b1aac4c6a7f81889c5fab160b79f48f2000c
chaseharmon <chase1harmon@gmail.com>
2024-12-07 02:39:32 -0500

    decode solver

--- scripts/run_decode_solver.sh
+++ scripts/run_decode_solver.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=2 python -u run_decode_solver.py \
---model_dir diffusion_models/{name-of-model} \
+--model_dir diffusion_models/diffuseq_EnglishSlang_h128_lr0.0001_t2000_sqrt_lossaware_seed102_learned_mask_fp16_denoise_0.5_reproduce20241207-04:26:36 \
 --seed 110 \
 --bsz 100 \
 --step 10 \
